81cc9fd2db16c349dae699e758cd2b3b1832dd39
anandshakya6398 <prabhatshakya166@gmail.com>
2025-06-15 07:35:38 +0530

    merge two array

---
+++ inpractice/Array/merge.js
@@ -0,0 +1,10 @@
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+
+// Using concat
+let merged = arr1.concat(arr2);
+console.log(merged); 
+
+// Using spread operator
+let mergedSpread = [...arr1, ...arr2];
+console.log(mergedSpread); 
