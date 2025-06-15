ca6c620b27aaf2679466eb71483466f1d19c28b2
anandshakya6398 <prabhatshakya166@gmail.com>
2025-06-15 07:34:45 +0530

    sort array

---
+++ inpractice/Array/sort.js
@@ -0,0 +1,8 @@
+let numbers = [4, 1, 3, 8, 5];
+
+// Ascending order
+numbers.sort((a, b) => a - b);
+console.log(numbers);
+// Descending order
+numbers.sort((a, b) => b - a);
+console.log(numbers); 
