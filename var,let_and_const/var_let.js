c458b2bd138ab6cfacc2746ad23d0b0d912b45d2
anandshakya6398 <prabhatshakya166@gmail.com>
2025-07-26 15:52:27 +0530

    important point in let_and_var

---
+++ var,let_and_const/var_let.js
@@ -0,0 +1,9 @@
+// console.log("a",a); // ReferenceError: Cannot access 'a' before initialization
+// let a=10;
+// console.log("a",a); 
+
+
+// var 
+console.log("b",b); // undefined
+var b = 20;
+console.log("b",b); // 20
