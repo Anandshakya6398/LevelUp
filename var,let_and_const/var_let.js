9e68cadaeee5954702fc189e3c2c2bca688fbd6e
anandshakya6398 <prabhatshakya166@gmail.com>
2025-07-26 16:12:31 +0530

    shadowing concept

--- var,let_and_const/var_let.js
+++ var,let_and_const/var_let.js
@@ -7,3 +7,22 @@
 console.log("b",b); // undefined
 var b = 20;
 console.log("b",b); // 20
+
+
+//shadowing concept 
+
+var a=10;
+console.log("a",a); // 10
+{
+    let a=30;
+    console.log("a",a); // 30
+}
+console.log("a",a); // 10
+
+
+let c=10;
+console.log("c",c); // 10
+{
+    var c=30; // var does not create a new block scope, it hoists to the top of the function or global scope
+    console.log("c",c); // 30
+}
