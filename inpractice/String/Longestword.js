a74f82aba4b90cd509334d36448c5e5460f7d86c
anandshakya6398 <prabhatshakya166@gmail.com>
2026-01-02 14:19:43 +0530

    initial commit

---
+++ inpractice/String/Longestword.js
@@ -0,0 +1,11 @@
+console.log(findLongetstWord("The quick brown fox jumped over the lazy dog"));
+// function findLongetstWord(str) {
+//     let words = str.split(' '); // Split the string into an array of words
+//     let longestWord = ''; // Initialize a variable to store the longest word
+//     for (let i = 0; i < words.length; i++) {
+//         if (words[i].length > longestWord.length) {
+//             longestWord = words[i]; // Update longestWord if the current word is longer
+//         }
+//     }
+//     return longestWord; // Return the longest word found
+// }   
