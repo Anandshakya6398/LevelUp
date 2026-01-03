2f7798003fc0944c8f96b3d28ffc2264bd9ace42
anandshakya6398 <prabhatshakya166@gmail.com>
2026-01-03 13:14:59 +0530

    add new method to find the lagest value in array

--- inpractice/Array/findMAX_MINvalue.js
+++ inpractice/Array/findMAX_MINvalue.js
@@ -18,3 +18,18 @@ console.log("maximum value")
 const arr2 = [5, 3, 9, 1, 7];
 const largest = Math.max(...arr2);
 console.log(largest);
+
+// second method >>>>>>>>>>>>>>>>>>>>>>.
+// find largest value in array 
+console.log("maximum value method 2")
+const getLargestValue = (arr3) => {
+    let largestvalue = 0;
+    for (let i = 0; i < arr3.length; i++) {
+        if (arr3[i] > largestvalue) {
+            largestvalue = arr3[i];
+        }
+    }
+    return largestvalue;
+};
+const arr3 = [5, 3, 9, 1, 7];
+console.log(getLargestValue(arr3)); // Output: 9
